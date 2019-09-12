6d41dea7f873515b28e9fbefc95f9e01bbd0084e
kuralarasu <kuralarasus@gmail.com>
2019-09-12 19:33:51 +0530

    Added the comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,13 @@ import {MatTableDataSource} from '@angular/material/table';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private bankService:BankingService) {}
+  constructor(private bankService: BankingService) {}
   title = 'BankApp';
   selectedCity = 'BANGALORE';
 
   // To set the city values in dropdown
   city: City[] = [
-    {value: 'BANGALORE', viewValue: 'BENGALURU'},
+    {value: 'BANGALORE', viewValue: 'BANGALORE'},
     {value: 'MUMBAI', viewValue: 'MUMBAI'},
     {value: 'CHENNAI', viewValue: 'CHENNAI'},
     {value: 'DELHI', viewValue: 'DELHI'},
@@ -29,22 +29,28 @@ export class AppComponent implements OnInit {
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
-  ngOnInit() {  
+  ngOnInit() {
     this.dataSource.paginator = this.paginator;
     this.getBankDetails(this.selectedCity);
   }
 
+  // To get the bank details based on the city selection
 getBankDetails(city) {
   this.bankService.getBankDetails(city).subscribe(res=>this.setBankDetails(res));
 }
 
+// To set the bank details to the table
 setBankDetails(res) {
-  this.dataSource=new MatTableDataSource(res);
-  this.dataSource.paginator=this.paginator;
+  this.dataSource = new MatTableDataSource(res);
+  this.dataSource.paginator = this.paginator;
 }
+
+// To filter the data
 applyFilter(filterValue: string) {
   this.dataSource.filter = filterValue.trim().toLowerCase();
 }
+
+// To show the bank details based on the selection of city
 eventSelection(event){
   this.getBankDetails(this.selectedCity);
  }
@@ -60,25 +66,3 @@ export interface City {
   value: string;
   viewValue: string;
 }
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-//   {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-//   {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-//   {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-//   {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-//   {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-//   {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-//   {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-//   {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-//   {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-//   {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-// ];
